0175f9d618fac2adca57648774710e5f4c11c234
thompsian <thompsian@gmail.com>
2019-11-25 13:04:34 -0800

    add listings

---
+++ apartment/app/javascript/components/Listing.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Listing extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p>This is where apartment listings will show</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Listing
