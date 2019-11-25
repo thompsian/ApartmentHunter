aa83223a67fe4688b57fe671cbdfec391e0d7df5
thompsian <thompsian@gmail.com>
2019-11-25 11:15:25 -0800

    Initial with devise

---
+++ apartment/app/javascript/components/Entry.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Entry extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <p>Create a new Apartment listing</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Entry
