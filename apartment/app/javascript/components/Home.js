fb9263894540716c82529fccb0c146bf1e49c187
thompsian <thompsian@gmail.com>
2019-11-25 19:10:38 -0800

    login to home

--- apartment/app/javascript/components/Home.js
+++ apartment/app/javascript/components/Home.js
@@ -1,10 +1,47 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import Entry from './pages/Entry'
+import Listing from './pages/Listing'
+
 class Home extends React.Component {
   render () {
+    const {
+      logged_in,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+
     return (
       <React.Fragment>
-        <h1>Apartment Finder</h1>
+        <Router>
+          <h1>Apartment Finder</h1>
+          <div className="Nav">
+            <div>
+              <Link to="/">Home</Link>
+            </div>
+            {logged_in &&
+              <div>
+                <Link to="./Entry" >New Apartment</Link>
+                <a href={sign_out_route}>Log Out</a>
+              </div>
+            }
+            {!logged_in &&
+              <div>
+                <a href={sign_in_route}>Log In</a>
+              </div>
+            }
+          </div>
+
+          <div>
+            < Listing />
+          </div>
+        </Router>
       </React.Fragment>
     );
   }
