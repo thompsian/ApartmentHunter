a47baf9a942f1034843ca3fa906e67b8a764b0dd
thompsian <thompsian@gmail.com>
2019-11-26 11:51:19 -0800

    Delete/Update API

--- apartment/app/controllers/listings_controller.rb
+++ apartment/app/controllers/listings_controller.rb
@@ -13,6 +13,25 @@ class ListingsController < ApplicationController
     end
   end
 
+  def update
+    listing = Listing.find(params[:id])
+    if listing.update(listing_params)
+      render json: listing, status: 201
+    else
+      render json: listing.errors, status: 422
+    end
+  end
+
+  def destroy
+    listing = Listing.find params[:id]
+    if listing.destroy
+        render json: listing
+    else
+        render json: {error: 'could not delete'}, status: 400
+    end
+end
+
+  private
   def listing_params
     params.require(:listing).permit(:street, :city, :zip)
   end
